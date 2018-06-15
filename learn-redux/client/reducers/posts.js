4020646be7a44eb9435c5f27d5fe8cc22964e7f6
Dmytro Yanushkevych <onswarm@gmail.com>
2018-06-15 19:11:30 +0300

    create reducers

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,11 @@
+// reducer:
+
+// 1. the action (what happened)
+// 2. copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default post;
